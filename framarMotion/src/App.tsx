8584885e1e44675b1ce497dcbe4961c96ae14925
Manu <manojc6363530865@gmail.com>
2025-07-17 22:49:37 +0530

    complete framer motion

--- framarMotion/src/App.tsx
+++ framarMotion/src/App.tsx
@@ -1,19 +1,24 @@
 // import React from 'react'
-import { motion } from "motion/react"
-
-const box = {
-  width: 100,
-  height: 100,
-  background: "red",
-}
+// import { motion } from "motion/react";
+// import ScrollTriggered from "./components/motion";
+import SharedLayoutAnimation from "./components/SharedLayoutAnimation";
+// const box = {
+//   width: 100,
+//   height: 100,
+//   background: "red",
+// };
 const App = () => {
   return (
-     <motion.div
-            style={box}
-            animate={{ rotate: 360 }}
-            transition={{ duration: 1 }}
-        />
-  )
-}
+    <>
+      {/* <motion.div
+        style={box}
+        animate={{ rotate: 360 }}
+        transition={{ duration: 1 }}
+      />
+      <ScrollTriggered /> */}
+      <SharedLayoutAnimation />
+    </>
+  );
+};
 
-export default App
+export default App;
