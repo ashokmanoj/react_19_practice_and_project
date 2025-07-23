95dcdf1097d25c4c232e95bf86ef1ee91e0de2f4
Manu <manojc6363530865@gmail.com>
2025-07-23 20:10:27 +0530

    complete recall zustand librery

---
+++ Zustand/src/Zus.tsx
@@ -0,0 +1,15 @@
+import { useCounterStore } from "./store";
+
+const Zus = () => {
+  const increment = useCounterStore((state) => state.increment);
+  const decrement = useCounterStore((state) => state.decrement);
+
+  return (
+    <div>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+    </div>
+  );
+};
+
+export default Zus;
