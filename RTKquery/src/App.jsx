0e945551b940a85a6ed186bb816581210c8d4059
Manu <manojc6363530865@gmail.com>
2025-07-28 20:37:04 +0530

    done with redux-toolkit-query

--- RTKquery/src/App.jsx
+++ RTKquery/src/App.jsx
@@ -1,35 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import AllProducts from "./components/AllProducts";
+// import SpecificProduct from "./components/SpecificProduct";
+// import AddNewProduct from "./components/AddNewProduct";
+// import UpdateProduct from "./components/UpdateProduct";
+import DeleteProduct from "./components/DeleteProduct";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      {/* <AllProducts /> */}
+      {/* <SpecificProduct /> */}
+      {/* <AddNewProduct /> */}
+      {/* <UpdateProduct productId={4} /> */}
+      <DeleteProduct productId={5} />
+    </div>
+  );
+};
 
-export default App
+export default App;
